d0a7291f3de1a895e76ad2308f5159dad218a6db
Mariia Karalash <madkara13@gmail.com>
2021-05-24 21:09:31 +0300

    Installed Firebase. Added navigation between controllers, file with constants.

--- Lessons/Lessons/Lesson-15/Controllers/WelcomeViewController.swift
+++ Lessons/Lessons/Lesson-15/Controllers/WelcomeViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import CLTypingLabel
 
 class WelcomeViewController: UIViewController, ControllerHeader {
 
@@ -14,24 +15,14 @@ class WelcomeViewController: UIViewController, ControllerHeader {
     var imageProject: UIImage = #imageLiteral(resourceName: "YouAvatar")
     
     
-    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var titleLabel: CLTypingLabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        titleLabel.text = ""
+        titleLabel.text = K.appName
         
-        var charIndex = 0.0
-        let titleText = "⚡️FlashChat"
-        
-        for letter in titleText {
-            Timer.scheduledTimer(withTimeInterval: 0.1 * charIndex, repeats: false) { (timer) in
-                self.titleLabel.text?.append(letter)
-            }
-            charIndex += 1
-        }
-        
-       
+   
     }
     
 
---
+++ Lessons/Lessons/Lesson-15/Model/Message.swift
@@ -0,0 +1,13 @@
+//
+//  Message.swift
+//  Lessons
+//
+//  Created by intern on 24.05.2021.
+//
+
+import Foundation
+
+struct Message {
+    let sender: String
+    let body: String
+}
