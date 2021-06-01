bffc36c73f3f7e173ce8b700ac613f7261cb302e
Mariia Karalash <madkara13@gmail.com>
2021-06-01 17:58:03 +0300

    Update cells

--- Lessons/Lessons/Lesson-15/View/MessageCell.swift
+++ Lessons/Lessons/Lesson-15/View/MessageCell.swift
@@ -9,15 +9,15 @@ import UIKit
 
 class MessageCell: UITableViewCell {
     
-    @IBOutlet weak var messageBubble: UIView!
+//    @IBOutlet weak var messageBubble: UIView!
     @IBOutlet weak var label: UILabel!
-    @IBOutlet weak var rightImageView: UIImageView!
+//    @IBOutlet weak var rightImageView: UIImageView!
     
     
     override func awakeFromNib() {
         super.awakeFromNib()
         
-        //messageBubble.layer.cornerRadius = messageBubble.frame.size.height / 5
+        
         
     }
 
